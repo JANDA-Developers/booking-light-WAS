351be9db375637cea7313975bfb776360c3d0e01
James <jeamsdean8917@gmailcom>
2020-09-17 17:30:23 +0900

    dev 2020 09 17

--- src/component/ProductViewer/ProductViewer.tsx
+++ src/component/ProductViewer/ProductViewer.tsx
@@ -1,64 +1,125 @@
-import React from "react"
-import moment from "moment"
-import { JDicon } from "@janda-com/front"
-
+import React from "react";
+import moment from "moment";
+import { JDicon, JDbutton } from "@janda-com/front";
 
 type TItem = {
-    name:string;
-    count:number;
-}
+  name: string;
+  count: number;
+  total: number;
+};
 
 type TData = {
-    date: Date,
-    item: TItem
-}
+  date: Date;
+  item: TItem[];
+};
 
 interface IProps {
-    date: Date,
-    datas: TData[]
-    onDateChange: (date: Date) => void
+  listNum: number
+  date: Date;
+  datas: TData[];
+  onDateChange: (date: Date) => void;
 }
 
-const ProductViewer: React.FC<IProps> = ({ date, datas, onDateChange }) => {
-    const handleDateArrow = (positive: boolean) => {
-        // toDate 메서드를통해서 momnet객체에서 Date 객체로 변환가능
-        const nextDate = moment(date).add(positive ? 1 : -1).toDate()
-        onDateChange(nextDate)
-    }
+interface ITest {
+  name: string;
+  count: number;
+  total: number;
+}
 
-    const renderData = Array(7).fill(null).map((_, i) => {
-        const Date = moment(date).add(i - 3, "day").toDate()
-        const itemDatas = datas.filter(i => moment(date).isSame(i.date));
-        const items = itemDatas.map(i => i.item);
+const ProductViewer: React.FC<IProps> = ({ listNum, date, datas, onDateChange }) => {
 
-        console.log(date);
-        console.log(items);
+  const handleDateArrow = (positive: boolean) => {
+    // toDate 메서드를통해서 momnet객체에서 Date 객체로 변환가능
+    const nextDate = moment(date)
+      .add(positive ? 1 : -1)
+      .toDate();
+    onDateChange(nextDate);
 
-        return { Date, items };
-    })
+  };
 
-    return <div>
-        <div>
-            <JDicon icon="shortLeft" onClick={() => {
-                handleDateArrow(false)
-            }} />
-            <JDicon icon="shortRight" onClick={() => {
-                handleDateArrow(true)
-            }} />
-            {moment(date).format("YYYY.MM.DD")}
-        </div>
-        {renderData.map(date => {
-            return <div>
-                {date.items.map(i => {
-                    return <div>
-                        
-                        {/* 아이템 출력 */}
-                    </div>
+  const renderData = Array(7)
+    .fill(null)
+    .map((_, index) => {
+      const Date = moment(date)
+        .add(index - 3, "day")
+        .toDate();
+      const itemDatas = datas.filter((dayList) => {
+        return moment(Date).isSame(dayList.date, "day")
+      });
+      const items = itemDatas.map((i) => i.item);
+
+      return { Date, items };
+    });
+
+  return (
+    <div className="dashboard__saleStatus">
+      <div className="statusNav">
+        <span className="statusNav__date">
+          {moment(date).format("YYYY.MM.DD")}
+        </span>
+        <span className="statusNav__arrow">
+          <JDicon
+            icon="shortLeft"
+            className="statusPrev"
+            onClick={() => {
+              handleDateArrow(false);
+            }}
+          />
+          <JDicon
+            icon="shortRight"
+            className="statusNext"
+            onClick={() => {
+              handleDateArrow(true);
+            }}
+          />
+        </span>
+      </div>
+      <div className="statusListWrap">
+        {renderData.map((itemInfo) => {
+          let selectedDay = moment(date).isSame(itemInfo.Date, "day");
+          let dayInfo = moment(itemInfo.Date).format('YYYYMMDD');
+          let dataLeng = itemInfo.items.length;
+          return (
+            <div className="statusList" key={`status-${dayInfo}`}>
+              <div className={`statusList__date ${selectedDay && "selected"}`}>
+                <span>{moment(itemInfo.Date).format("DD")}</span>
+                <span>{moment(itemInfo.Date).format("ddd")}</span>
+              </div>
+              <ul className="statusList__itemBlock">
+                {itemInfo.items.map((item) => {
+                  return item.slice(0, listNum).map((infoList) => {
+                    return (
+                      <li className="items selected" key={`statusList-${infoList.name}`}>
+                        <div className="items__deco"></div>
+                        <div className="items__info">
+                          <div className="items__name">
+                            {infoList.name}
+                          </div>
+                          <div className="items__count">
+                            <span className="items__booked">
+                              {infoList.count}
+                            </span>
+                            <span> / </span>
+                            <span className="items__total">
+                              {infoList.total}
+                            </span>
+                          </div>
+                        </div>
+                      </li>
+                    );
+                  })
                 })}
+                {
+                  dataLeng != 0 ? <li>
+                    <a href="" className="statusMore">더보기</a></li> : false
+                }
+              </ul>
             </div>
+          );
         })}
+      </div>
     </div>
-}
+  );
+};
 
-
-export default ProductViewer
+export default ProductViewer;
--- src/page/dashboard/Dashboard.tsx
+++ src/page/dashboard/Dashboard.tsx
@@ -1,24 +1,59 @@
-import { JDcontainer } from "@janda-com/front"
-import React from "react"
-import ProductViewer from "../../component/ProductViewer/ProductViewer"
 
-const Dashboard = () => {
-    const datas = [
-        { date: new Date(), item: { name: "item 1", count: 1 } },
-        { date: new Date(), item: { name: "item 2", count: 2 } },
-        { date: new Date(), item: { name: "item 3", count: 3 } },
-        { date: new Date(), item: { name: "item 4", count: 4 } },
-        { date: new Date(), item: { name: "item 5", count: 5 } },
-        { date: new Date(), item: { name: "item 6", count: 6 } },
-        { date: new Date(), item: { name: "item 7", count: 7 } },
-    ];
+import React, { useState } from "react";
+import { JDcontainer, WindowSize } from "@janda-com/front";
+import ProductViewer from "../../component/ProductViewer/ProductViewer";
+import "../../scss/Dashboard/Dashboard.scss";
 
-    return <div>
-        <JDcontainer>
-            <ProductViewer date={new Date()} datas={datas} onDateChange={() => { }} />
-        </JDcontainer>
-    </div>
+interface IProps {
+  title: string;
+  subtitle: string;
 }
 
+const Dashboard: React.FC<IProps> = ({ title, subtitle }) => {
+  const [selectedDay, setselectedDay] = useState(new Date());
+  const yesterday = new Date();
+  yesterday.setDate(15)
 
-export default Dashboard
+  const onDateChange = (direction: string) => {
+
+  }
+
+  const datas = [
+    {
+      date: new Date(),
+      item: [
+        { name: "Room 1", count: 20, total: 100 },
+        { name: "Room 2", count: 30, total: 100 },
+        { name: "Room 3", count: 50, total: 100 }
+      ]
+    },
+    {
+      date: yesterday,
+      item: [
+        { name: "Room 1", count: 20, total: 100 },
+        { name: "Room 2", count: 30, total: 100 },
+        { name: "Room 3", count: 50, total: 100 }
+      ]
+    }
+  ];
+
+  return (
+    <JDcontainer size={WindowSize.full}>
+
+      <div className="dashboard">
+        <h1 className="dashboard__title">{title}</h1>
+        <h2 className="dashboard__subtitle">{subtitle}</h2>
+        <JDcontainer>
+          <ProductViewer
+            listNum={2}
+            date={selectedDay}
+            datas={datas}
+            onDateChange={() => { }}
+          />
+        </JDcontainer>
+      </div>
+    </JDcontainer>
+  );
+};
+
+export default Dashboard;
