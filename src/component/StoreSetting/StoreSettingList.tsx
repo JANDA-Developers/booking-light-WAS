b0f04b8ee297dfe182fcf11e2c54ea560b742132
James <jeamsdean8917@gmailcom>
2020-09-29 17:35:58 +0900

    2020 09 29 itemList, storeList completed

--- src/component/StoreSetting/StoreSettingList.tsx
+++ src/component/StoreSetting/StoreSettingList.tsx
@@ -71,57 +71,6 @@ const StoreSettingList: React.FC<IProps> = ({ index, image, name, address, goods
 
         </JDcard>
     )
-
-    // return (
-    //     <div>
-    //         {
-    //             storeList.map((storeItem) => {
-    //                 return <div className="storeSetting__list storeList">
-    //                     <div className="storeList__imageBlock">
-    //                         <div className="storeList__image" style={{ backgroundImage: `url(${storeItem.image})` }}></div>
-    //                     </div>
-    //                     <div className="storeList__infoBlock">
-    //                         <div className="infoBlock__name">
-    //                             <strong>{storeItem.name}</strong>
-    //                             <b><JDicon icon="location"></JDicon> {storeItem.address}</b>
-    //                         </div>
-    //                         <div className="infoBlock__sold">
-    //                             <div className="infoBlock__soldList">
-    //                                 <strong>상품</strong>
-    //                                 <span>{storeItem.goods}</span>
-    //                             </div>
-    //                             <div className="infoBlock__soldDeco"></div>
-    //                             <div className="infoBlock__soldList">
-    //                                 <strong>판매</strong>
-    //                                 <span>{storeItem.sold}</span>
-    //                             </div>
-    //                             <div className="infoBlock__soldDeco"></div>
-    //                             <div className="infoBlock__soldList">
-    //                                 <strong>생성일</strong>
-    //                                 <span>{moment(storeItem.generated).format('YYYY-MM-DD')}</span>
-    //                             </div>
-    //                             <div className="infoBlock__soldDeco"></div>
-    //                             <div className="infoBlock__soldList">
-    //                                 <strong>회원</strong>
-    //                                 <span>{storeItem.member}</span>
-    //                             </div>
-    //                         </div>
-    //                         <div className="infoBlock__desc">
-    //                             <p>
-    //                                 {storeItem.desc}
-    //                             </p>
-    //                         </div>
-    //                     </div>
-    //                     <div className="storeList__controlBlock">
-    //                         <button className="storeList__edit">수정하기</button>
-    //                         <button className="storeList__delete">삭제하기</button>
-    //                     </div>
-    //                 </div>
-    //             })
-    //         }
-    //     </div>
-    // )
-
 }
 
 export default StoreSettingList
